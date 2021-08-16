132d6e0bb3c736eb79e17ec5fc290c2fd601b81f
Younes AKHOUAYRI <younes.akhouayri@ibm.com>
2021-08-16 15:18:07 +0200

    * Subscribe to a channel : call the callback multiple times * Include workflowInstanceKey in variables * Add a cleanup function : unsubscribe from a channel and delete the associated callbacks * Packages upgrade : zeebe-node & uuid
    Signed-off-by: Younes <dev@younes.io>

--- src/RedisAffinity.ts
+++ src/RedisAffinity.ts
@@ -1,112 +1,131 @@
-import redis from 'redis';
+/* eslint-disable no-unused-vars */
+import redis, { ClientOpts, RedisClient } from 'redis';
 import { v4 as uuid } from 'uuid';
-import { ZBClient, Duration } from 'zeebe-node';
-import { KeyedObject } from 'zeebe-node/dist/lib/interfaces';
+import { Duration, KeyedObject, ZBClient } from 'zeebe-node';
 import { WorkflowOutcome } from "./WebSocketAPI";
-
-import { ClientOpts, RedisClient } from 'redis';
 
 // TODO: handle errors if missing parameters (example: workflow instance key)
 // TODO: purge the service
 
 export class RedisAffinity extends ZBClient {
-  private subscriber: RedisClient;
-  private publisher: RedisClient;
-  private affinityCallbacks: {
-    [workflowInstanceKey: string]: (workflowOutcome: WorkflowOutcome) => void;
-  };
+    public subscriber: RedisClient;
 
-  constructor(gatewayAddress: string, redisOptions: ClientOpts) {
-    super(gatewayAddress);
+    publisher: RedisClient;
 
-    this.subscriber = redis.createClient(redisOptions);
-    this.publisher = redis.createClient(redisOptions);
-    this.affinityCallbacks = {};
+    affinityCallbacks: {
+        // eslint-disable-next-line no-unused-vars
+        [workflowInstanceKey: string]: (workflowOutcome: WorkflowOutcome) => void;
+    };
 
-    this.subscriber.on('error', function (error) {
-      console.error(error);
-    });
-    this.publisher.on('error', function (error) {
-      console.error(error);
-    });
+    constructor(gatewayAddress: string, redisOptions: ClientOpts) {
+        super(gatewayAddress);
 
-    this.subscriber.on('message', (channel: string, message: string) => {
-      console.log('subscriber received message in channel ' + channel);
-      this.subscriber.unsubscribe(channel);
-      try {
-        this.affinityCallbacks[channel](JSON.parse(message));
-        delete this.affinityCallbacks[channel];
-      } catch (err) {
-        console.error(err);
-      }
-    });
-  }
+        this.subscriber = redis.createClient(redisOptions);
+        this.publisher = redis.createClient(redisOptions);
+        this.affinityCallbacks = {};
 
-  async createAffinityWorker(taskType: string) {
-    const workerId: string = uuid();
+        this.subscriber.on('connected', (error) => {
+            console.log('Subscriber connected');
+        });
+        this.publisher.on('connected', (error) => {
+            console.log('Publisher connected');
+        });
+        this.subscriber.on('error', (error) => {
+            console.error(error);
+        });
+        this.publisher.on('error', (error) => {
+            console.error(error);
+        });
 
-    // create worker (ZB client)
-    super.createWorker(workerId, taskType, async (job, complete) => {
-      console.log('Publish message on channel: ' + job.workflowInstanceKey);
-      this.publisher.publish(job.workflowInstanceKey, JSON.stringify(job.variables));
-      complete.success();
-    });
-  }
-
-  async createWorkflowInstanceWithAffinity<Variables = KeyedObject>({
-    bpmnProcessId,
-    variables,
-    cb,
-  }: {
-    bpmnProcessId: string;
-    variables: Variables;
-    cb: (workflowOutcome: WorkflowOutcome) => void;
-  }) {
-    try {
-      // create workflow instance (ZB client)
-      const wfi = await super.createWorkflowInstance(bpmnProcessId, variables);
-
-      this.affinityCallbacks[wfi.workflowInstanceKey] = cb;
-
-      this.subscriber.subscribe(wfi.workflowInstanceKey, () => {
-        console.log(
-          'Subscribe to channel ' + wfi.workflowInstanceKey,
-        );
-      });
-    } catch (err) {
-      console.error(err);
-      throw err;
+        this.subscriber.on('message', (channel: string, message: string) => {
+            console.log(`subscriber received message in channel ${channel}`);
+            try {
+                this.affinityCallbacks[channel](JSON.parse(message));
+            } catch (err) {
+                console.error(err);
+            }
+        });
     }
-  }
 
-  async publishMessageWithAffinity<Variables = KeyedObject>({
-    correlationKey,
-    messageId,
-    name,
-    variables,
-    workflowInstanceKey,
-    cb,
-  }: {
-    correlationKey: string;
-    messageId: string;
-    name: string;
-    variables: Variables;
-    workflowInstanceKey: string;
-    cb: (workflowOutcome: WorkflowOutcome) => void;
-  }) {
-    super.publishMessage({
-      correlationKey,
-      messageId,
-      name,
-      variables,
-      timeToLive: Duration.seconds.of(10),
-    });
+    async createAffinityWorker(taskType: string): Promise<void> {
+        const workerId: string = uuid();
 
-    this.affinityCallbacks[workflowInstanceKey] = cb;
+        // create worker (ZB client)
+        super.createWorker(workerId, taskType, async (job, complete) => {
+            console.log(`Publish message on channel: ${job.workflowInstanceKey}`);
+            const updatedVars = {
+                ...job?.variables,
+                workflowInstanceKey: job?.workflowInstanceKey,
+            };
+            this.publisher.publish(job.workflowInstanceKey, JSON.stringify(updatedVars));
+            await complete.success(updatedVars);
+        });
+    }
 
-    // TODO: add error message if missing workflowInstanceKey
-    this.subscriber.subscribe(workflowInstanceKey, () => {
-      console.log('Subscribe to channel ' + workflowInstanceKey);
-    });
-  }
+    async createWorkflowInstanceWithAffinity<Variables = KeyedObject>({
+        bpmnProcessId,
+        variables,
+        cb,
+    }: {
+        bpmnProcessId: string;
+        variables: Variables;
+        cb: (workflowOutcome: WorkflowOutcome) => void;
+    }): Promise<void> {
+        try {
+            // create workflow instance (ZB client)
+            const wfi = await super.createWorkflowInstance(bpmnProcessId, variables);
+
+            this.affinityCallbacks[wfi.workflowInstanceKey] = cb;
+
+            this.subscriber.subscribe(wfi.workflowInstanceKey, () => {
+                console.log(`Subscribe to channel ${wfi.workflowInstanceKey}`);
+            });
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    }
+
+    async publishMessageWithAffinity<Variables = KeyedObject>({
+        correlationKey,
+        messageId,
+        name,
+        variables,
+        workflowInstanceKey,
+        cb,
+    }: {
+        correlationKey: string;
+        messageId: string;
+        name: string;
+        variables: Variables;
+        workflowInstanceKey: string;
+        cb: (workflowOutcome: WorkflowOutcome) => void;
+    }): Promise<void> {
+        await super.publishMessage({
+            correlationKey,
+            messageId,
+            name,
+            variables,
+            timeToLive: Duration.seconds.of(10),
+        });
+
+        this.affinityCallbacks[workflowInstanceKey] = cb;
+
+        // TODO: add error message if missing workflowInstanceKey
+        this.subscriber.subscribe(workflowInstanceKey, () => {
+            console.log(`Subscribe to channel ${workflowInstanceKey}`);
+        });
+    }
+
+    cleanup(channel: string): void {
+        console.log(
+            `Unsubscribe from channel ${channel} and removing affinity callbacks.`,
+        );
+        this.subscriber.unsubscribe(channel);
+        try {
+            delete this.affinityCallbacks[channel];
+        } catch (err) {
+            console.error(err);
+        }
+    }
 }
